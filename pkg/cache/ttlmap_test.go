1886fae4df678d9ecd4421707828f5c145358dd3
dongzhiwei <2660847388@qq.com>
2023-07-20 10:23:47 +0800

    [Unit]  Improve cache unit testing

--- pkg/cache/ttlmap_test.go
+++ pkg/cache/ttlmap_test.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestTtlMapCache_Set(t *testing.T) {
@@ -12,4 +13,8 @@ func TestTtlMapCache_Set(t *testing.T) {
 	err := ttl.Get("name", &name)
 	assert.Nil(t, err)
 	assert.Equal(t, "king", name)
+	time.Sleep(time.Second * 3)
+	err = ttl.Get("name", &name)
+	// 3s过期
+	assert.EqualError(t, err, "cache value of key is not found")
 }
